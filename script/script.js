b80ecc3fc7fee062171da5f705ef5f6d8f4ccd52
Josh Rogerson <jrogerson3@my.bcit.ca>
2022-11-11 16:27:41 -0800

    Defined a constant arrat called Courselist with code and name properties

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const courseList = [
+  {code: "ACIT 1620", name: "Web Fundamental Technologies"},
+  {code: "COMM 1116", name: "Business Communications"},
+  {code: "Math 1310", name: "Technical Math for IT"}
+  
+]
+
+
+
